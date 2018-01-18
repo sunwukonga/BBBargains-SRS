9327ec305e12e190aa6802e32d32bcacb2cf28f7
Paul Desmond Parker <pauldesmondparker@gmail.com>
2018-01-19 00:26:45 +0800

    Allow script to push back to repo

--- publish.sh
+++ publish.sh
@@ -46,7 +46,8 @@ if git diff --staged --quiet; then
   echo "Nothing to publish"
 else
   git commit -a -m "Deploy to GitHub Pages"
-  git push
+  git push "https://${GH_TOKEN}@github.com/sunwukonga/BBBargains-SRS.git" gh-pages
+
   #git push > /dev/null 2>&1
   echo "Pushed"
 fi
